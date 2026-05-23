5668d942858312ab4702ab88f9a0d77434970d12
nicoboga4075 <nicolas.bogalheiro@gmail.com>
2026-05-23 12:26:28 +0200

    Release v1.0.0

---
+++ update-version.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+const version = process.argv[2];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const d = new Date();
+const releaseDate = process.argv[3] || String(d.getDate()).padStart(2, '0') + ' ' + months[d.getMonth()] + ' ' + d.getFullYear();
+
+if (!version) {
+  console.error('Usage: node update-version.js <version> [releaseDate]');
+  process.exit(1);
+}
+
+// Update popup.html
+const htmlPath = path.join(__dirname, 'popup.html');
+let html = fs.readFileSync(htmlPath, 'utf8');
+html = html.replace(
+  html.match(/Version : <span class="hl">[^<]*<\/span>/)[0],
+  `Version : <span class="hl">${version}</span>`
+);
+html = html.replace(
+  html.match(/Release : <span class="hl">[^<]*<\/span>/)[0],
+  `Release : <span class="hl">${releaseDate}</span>`
+);
+fs.writeFileSync(htmlPath, html);
+console.log(`popup.html updated -> v${version} / ${releaseDate}`);
+
+// Update CHANGELOG.md
+const changelogPath = path.join(__dirname, 'CHANGELOG.md');
+let changelog = fs.readFileSync(changelogPath, 'utf8');
+changelog = changelog.replace(
+  new RegExp(`(#### v${version.replace(/\./g, '\\.')}\\r\\n\\r\\n> )[^\\r\\n]*`),
+  `$1${releaseDate}`
+);
+fs.writeFileSync(changelogPath, changelog);
+console.log(`CHANGELOG.md updated -> v${version} / ${releaseDate}`);
